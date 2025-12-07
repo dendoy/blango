d2951dee0f9859e3bd541fca01e869d7879ef2d3
Dendy Ginanjar <2003dendy@gmail.com>
2025-12-07 13:20:29 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from blog.forms import CommentForm
 import logging
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 logger = logging.getLogger(__name__)
 
